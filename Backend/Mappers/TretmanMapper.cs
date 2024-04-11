edaa0e06fd18c0c985c74f5f80b4170269fd9f69
Dajana <dajanabozic@gmail.com>
2024-04-11 20:43:19 +0200

    Update TretmanMapper.cs

--- Backend/Mappers/TretmanMapper.cs
+++ Backend/Mappers/TretmanMapper.cs
@@ -1,6 +1,47 @@
-﻿namespace Backend.Mappers
+﻿using Backend.Models;
+
+namespace Backend.Mappers
 {
-    public class TretmanMapper
-    {
+   
+        public class TretmanMapper : Mapping<Tretman, TretmanDTORead, TretmanDTOInsertUpdate>
+        {
+
+            public TretmanMapper()
+            {
+                MapperMapReadToDTO = new Mapper(new MapperConfiguration(c => {
+                    c.CreateMap<Tretman, TretmanDTORead>()
+                    .ConstructUsing(entitet =>
+                     new TretmanDTORead(
+                        entitet.id,
+                        
+                        entitet.Datum,
+                        entitet.Korisnik
+                       
+                        ));
+                }));
+
+                MapperMapInsertUpdatedFromDTO = new Mapper(new MapperConfiguration(c => {
+                    c.CreateMap<TretmanDTOInsertUpdate, Tretman>();
+                }));
+
+                MapperMapInsertUpdateToDTO = new Mapper(new MapperConfiguration(c => {
+                    c.CreateMap<Tretman, TretmanDTOInsertUpdate>()
+                    .ConstructUsing(entitet =>
+                     new TretmanDTOInsertUpdate(
+                          entitet.id,
+
+                        entitet.Datum,
+                        entitet.Korisnik
+                        ));
+
+
+
+
+                }));
+            }
+
+
+
+
+        }
     }
-}
